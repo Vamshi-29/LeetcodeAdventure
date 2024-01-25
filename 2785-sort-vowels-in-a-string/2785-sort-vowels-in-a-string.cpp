03bff376d1c622d273db602381211e6ce1af8254
Vamshi <157635591+Vamshi-29@users.noreply.github.com>
2024-01-26 01:04:48 +0530

    Time: 55 ms (25.44%), Space: 14.7 MB (30.42%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
@@ -1,49 +1,28 @@
 class Solution {
 public:
     string sortVowels(string s) {
-        string capvowel;
-        string smallvowel;
         int n=s.length();
+        unordered_map<char,int>um;
         for(int i=0;i<n;i++)
         {
             if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u')
-            smallvowel+=s[i];
+            um[s[i]]++;
             else if(s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U')
-            capvowel+=s[i];
+            um[s[i]]++;
         }
-        sort(capvowel.begin(),capvowel.end(),greater<char>());
-        sort(smallvowel.begin(),smallvowel.end(),greater<char>());
-        int m1=capvowel.length()-1,i=0;
-        while(m1>=0 && i<n)
+        string ans="AEIOUaeiou";
+        int j=0;
+        for(int i=0;i<n;i++)
         {
-            if(s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U')
+            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' || s[i]=='A' || s[i]       =='E' || s[i]=='I' || s[i]=='O' || s[i]=='U')
             {
-                s[i]=capvowel[m1];
-                m1--;
+                while(um[ans[j]]==0)
+                {
+                    j=j+1;
+                }
+                s[i]=ans[j];
+                um[ans[j]]--;
             }
-            else if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u')
-            {
-                s[i]=capvowel[m1];
-                m1--;
-            }
-            i++;
-        }
-        m1=smallvowel.length()-1;
-        cout<<capvowel<<endl;
-        cout<<smallvowel<<endl;
-        while(m1>=0 && i<n)
-        {
-            if(s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U')
-            {
-                s[i]=smallvowel[m1];
-                m1--;
-            }
-            else if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u')
-            {
-                s[i]=smallvowel[m1];
-                m1--;
-            }
-            i++;
         }
         return s;
     }
