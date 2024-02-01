c8064ed6032ea6e0ec027ce486f212d826c1c5b4
Vamshi <157635591+Vamshi-29@users.noreply.github.com>
2024-02-01 13:31:42 +0530

    Time: 4 ms (86.73%), Space: 18 MB (9.28%) - LeetHub

--- 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
@@ -5,18 +5,14 @@ public:
         int n=pushed.size();
         int i=0,j=0;
         while(i<n){
+            s.push(pushed[i]);
             while(!s.empty() && s.top()==popped[j])
             {
                 s.pop();
                 j++;
             }
-            s.push(pushed[i]);
+          
             i++;
-        }
-        while(!s.empty() && s.top()==popped[j])
-        {
-            s.pop();
-            j++;
         }
         if(!s.empty())
         return false;
