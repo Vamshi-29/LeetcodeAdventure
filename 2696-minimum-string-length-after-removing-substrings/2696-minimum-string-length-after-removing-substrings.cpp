d4480e232a57ae7822633375c458cc517ad5bca0
Vamshi <157635591+Vamshi-29@users.noreply.github.com>
2024-02-20 17:23:13 +0530

    Time: 0 ms (100%), Space: 9.1 MB (50.53%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
     int minLength(string s) {
-        stack<char>sta;
+        string str;
         int n=s.length();
         for(int i=0;i<n;i++)
         {
-           if(s[i]=='B' && !sta.empty())
-           {
-               if(sta.top()=='A')
-               sta.pop();
-               else
-               sta.push(s[i]);
-           }
-           else if(s[i]=='D' && !sta.empty())
-           {
-               if(sta.top()=='C')
-               sta.pop();
-                else
-                sta.push(s[i]);
-           }
-           else
-           sta.push(s[i]);
+            if(s[i]=='B' && !str.empty() && str.back()=='A')
+            {
+                str.pop_back();
+            }
+            else if(s[i]=='D' && !str.empty() && str.back()=='C')
+            {
+                str.pop_back();
+            }
+            else
+            str+=s[i];
         }
-        return sta.size();
+        return str.length();
+
     }
 };
