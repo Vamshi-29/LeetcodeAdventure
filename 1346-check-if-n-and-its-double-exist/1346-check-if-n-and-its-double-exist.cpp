9e29a7b105dd015b632ad4ef4dbed98e485572b2
Vamshi <157635591+Vamshi-29@users.noreply.github.com>
2024-02-21 18:49:12 +0530

    Time: 8 ms (39.11%), Space: 14.9 MB (6.2%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
-        sort(arr.begin(),arr.end());
+        unordered_map<int,int>um;
         int n=arr.size();
         for(int i=0;i<n;i++)
+        um[arr[i]]=i;
+        for(int i=0;i<n;i++)
         {
-            int start=0;
-            int end=n-1;
-            while(start<=end)
-            {
-                int mid=(start)+(end-start)/2;
-                if(arr[mid]==2*arr[i] && mid!=i)
-                return true;
-                else if(arr[mid] > 2*arr[i])
-                end=mid-1;
-                else
-                start=mid+1;
-            }
+            if(um.find(2*arr[i])!=um.end() && um[2*arr[i]]!=i)
+            return true;
         }
         return false;
     }
