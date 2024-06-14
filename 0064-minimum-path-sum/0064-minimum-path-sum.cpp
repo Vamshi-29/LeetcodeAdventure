0c3f862d405081300c5c254368d4129044d3af5f
Vamshi <157635591+Vamshi-29@users.noreply.github.com>
2024-06-14 19:24:05 +0530

    Time: 8 ms (34.42%), Space: 12.6 MB (33.35%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,46 +1,30 @@
 class Solution {
 public:
-int gridmin(vector<vector<int>>grid,int rows,int cols,vector<vector<int>>&dp)
-{
-    if(rows>=0 && cols>=0 && dp[rows][cols]!=-1 )
-    return dp[rows][cols];
-    if(rows==0 && cols==0)
-    return grid[0][0];
-    if(rows<0 || cols<0)
-    return int(1e9);
-    int up=grid[rows][cols]+gridmin(grid,rows-1,cols,dp);
-    int side=grid[rows][cols]+gridmin(grid,rows,cols-1,dp);
-    return dp[rows][cols]=min(up,side);
-}
-    int minPathSum(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        
-       vector<vector<int>> dp(m,vector<int>(n, 0));
-      
-        // return gridmin(grid,m-1,n-1,dp);
-        dp[0][0]=grid[0][0];
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(i==0 && j==0)
-                continue;
-                int up=0;
-                int down=0;
-                
-                up=grid[i][j];
-                if(i>0)
-                up+=dp[i-1][j];
-                else
-                up+=1e9;
-                down=grid[i][j];
-                if(j>0) down+=dp[i][j-1];
-                else
-                down+=1e9;
-                dp[i][j]= min(up, down);
-            }
+     int minipath(vector<vector<int>>& grid, int down, int right, int n, int m,vector<vector<int>>&dp) {
+        if (down == n - 1 && right == m - 1) {
+            return grid[down][right];
         }
-        return dp[m-1][n-1];
+        if(down!=n-1 && right!=m-1){
+        if(dp[down][right]!=-1)
+        return dp[down][right];
+        }
+        int downsum = INT_MAX, rightsum = INT_MAX;
+        
+        if (down != n - 1) {
+            downsum = grid[down][right] + minipath(grid, down + 1, right, n, m,dp);
+        }
+        
+        if (right != m - 1) {
+            rightsum = grid[down][right] + minipath(grid, down, right + 1, n, m,dp);
+        }
+        if(down!=n-1 && right!=m-1)
+        return dp[down][right]=min(downsum, rightsum);
+        return min(downsum,rightsum);
+    }
+    int minPathSum(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1, -1));
+        return minipath(grid,0,0,n,m,dp);
     }
 };
