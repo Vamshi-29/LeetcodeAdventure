f3df0a695cdb945bf819e926cd248cf678948442
Vamshi <157635591+Vamshi-29@users.noreply.github.com>
2024-06-14 20:38:06 +0530

    Time: 131 ms (67.75%), Space: 98.7 MB (39.25%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,45 +1,35 @@
 class Solution {
 public:
-    bool equalsubset(vector<int>nums,int sum,int index,vector<vector<bool>>&dp)
-    {
-        if(dp[index][sum]!=false)
-        return dp[index][sum];
-        if(sum==0)
+    bool subsetSumUtil(int ind, int target, vector<int>& arr, vector<vector<int>>& dp) {
+    if (target == 0)
         return true;
-        if(index==0)
-        return nums[0]==sum;
-        bool notpick=equalsubset(nums,sum,index-1,dp);
-        bool pick=false;
-        if(nums[index]<=sum)
-        pick=equalsubset(nums,sum-nums[index],index-1,dp);
-        return dp[index][sum]=pick || notpick;
-    }
-    bool canPartition(vector<int>& nums) {
-        int sum=0;
-        int n=nums.size();
-        for(int i=0;i<nums.size();i++)
-        sum+=nums[i];
-        if(sum%2==1)
-        return false;
-        sum/=2;
-        vector<vector<bool>> dp(n, vector<bool>(sum + 1, false));
-        //return equalsubset(nums,sum,nums.size()-1,dp);
-        for(int i=0;i<n;i++)
-        dp[i][0]=true;
-        if (nums[0] <= sum)
-            dp[0][nums[0]] = true;
 
-        for(int i=1;i<n;i++)
-        {
-            for(int target=1;target<=sum;target++)
-            {
-                bool notpick=dp[i-1][target];
-                bool pick=false;
-                if(nums[i]<=target)
-                pick=dp[i-1][target-nums[i]];
-                dp[i][target]= pick || notpick;
-            }
-        }
-        return dp[n-1][sum];
+    if (ind == 0)
+        return arr[0] == target;
+
+    if (dp[ind][target] != -1)
+        return dp[ind][target];
+
+    bool notTaken = subsetSumUtil(ind - 1, target, arr, dp);
+
+    bool taken = false;
+    if (arr[ind] <= target)
+        taken = subsetSumUtil(ind - 1, target - arr[ind], arr, dp);
+
+    return dp[ind][target] = notTaken || taken;
+    }
+    bool canPartition(vector<int>& arr) {
+    int totSum = 0;
+    int n=arr.size();
+    for (int i = 0; i < n; i++) {
+        totSum += arr[i];
+    }
+
+    if (totSum % 2 == 1)
+        return false;
+   
+        int k = totSum / 2;
+        vector<vector<int>> dp(n, vector<int>(k + 1, -1));
+        return subsetSumUtil(n - 1, k, arr, dp);
     }
 };
