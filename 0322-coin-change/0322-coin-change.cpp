bb5428e01ea19c87638535008aed5d6f2a0a679b
Vamshi <157635591+Vamshi-29@users.noreply.github.com>
2024-06-17 02:54:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+
+int  getsol(vector<int>coins,int target,int index,int n,vector<vector<int>>&dp)
+{
+    if(target==0)
+    {
+        return 0;
+    }
+    if(index>=n)
+        return INT_MAX-999;
+    if(dp[index][target]!=-1)
+        return dp[index][target];
+    int pick=INT_MAX;
+    if(target>=coins[index])
+    pick=1+getsol(coins,target-coins[index],index,n,dp);
+    int notpick=getsol(coins,target,index+1,n,dp);
+    return dp[index][target]=min(pick,notpick);
+}
+int coinChange(vector<int>& coins, int amount) {
+    int n=coins.size();
+    vector<vector<int>>dp(n, vector<int>(amount + 1, -1));
+    int ans=getsol(coins,amount,0,coins.size(),dp);
+    if(ans==INT_MAX-999)
+    return -1;
+    return ans;
+}
+};
