d78c3aee4c40eb86dfe7c6598403615a03430562
Vamshi <157635591+Vamshi-29@users.noreply.github.com>
2024-06-18 17:53:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+int getsol(string word1,string word2,int end1,int end2,vector<vector<int>>&dp)
+    {
+        if(end1<0 && end2<0)
+        return 0;
+        if(end1<0 || end2<0)
+        {
+            return max(end1,end2)+1;
+        }
+        if(dp[end1][end2]!=-1)
+        return dp[end1][end2];
+        if(word1[end1]==word2[end2])
+        {
+            return dp[end1][end2]=getsol(word1,word2,end1-1,end2-1,dp);
+        }
+        else
+        {
+           return dp[end1][end2]=1+min(getsol(word1,word2,end1-1,end2,dp),getsol(word1,word2,end1,end2-1,dp));
+        }
+    }
+    int minDistance(string word1, string word2) {
+        int n=word1.length();
+        int m=word2.length();
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        return getsol(word1,word2,n-1,m-1,dp);
+    }
+    
+};
