3a8ca87d761593f6064597df83ab3a488ea369f3
Vamshi <157635591+Vamshi-29@users.noreply.github.com>
2024-06-24 14:12:14 +0530

    Time: 9 ms (30.02%), Space: 21 MB (7.19%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
-    int getans(vector<int> prices, int index, int status, int n,vector<vector<int>>&dp) {
-        if (index >= n)
+    int getans(const vector<int>& prices, int index, int status, int n, vector<vector<int>>& dp, int maxDepth) {
+        if (index >= n || maxDepth <= 0)
             return 0;
-        if(dp[index][status]!=-1)
-        return dp[index][status];
+        if (dp[index][status] != -1)
+            return dp[index][status];
+
         int profit = 0;
         if (status == 0) { 
-            profit = max(0, max(-prices[index] + getans(prices, index + 1, 1, n,dp), getans(prices, index + 1, 0, n,dp)));
+            profit = max(0, max(-prices[index] + getans(prices, index + 1, 1, n, dp, maxDepth - 1), getans(prices, index + 1, 0, n, dp, maxDepth - 1)));
         } else { 
-            profit = max(0, max(prices[index] + getans(prices, index + 1, 0, n,dp), getans(prices, index + 1, 1, n,dp)));
+            profit = max(0, max(prices[index] + getans(prices, index + 1, 0, n, dp, maxDepth - 1), getans(prices, index + 1, 1, n, dp, maxDepth - 1)));
         }
-        return dp[index][status]=profit;
+
+        return dp[index][status] = profit;
     }
 
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>>dp(n,vector<int>(2,-1));
-        return getans(prices, 0, 0, n,dp);
+        vector<vector<int>> dp(n, vector<int>(2, -1));
+        int maxDepth = n; 
+        return getans(prices, 0, 0, n, dp, maxDepth);
     }
 };
+
+
