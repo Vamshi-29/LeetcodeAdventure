ef2a53f67bb686b95b846ea874c27cde0159a961
Vamshi <157635591+Vamshi-29@users.noreply.github.com>
2024-06-26 00:05:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    int getsol(vector<vector<int>>& obs,int row,int col,vector<vector<int>>&dp)
+    int cnt=0;
+    int getans(vector<vector<int>>&grid,int row,int col,int n,int m)
     {
-        if(row==0 && col==0 && obs[row][col]!=1)
-        {
-            return 1;
+        if(row==n-1 && col==m-1 && grid[row][col]==0){
+        return 1;
         }
-        if(dp[row][col]!=-1)
-        return dp[row][col];
-        if(obs[row][col]==1)
+        if(grid[row][col]==1)
         return 0;
-        int up=0,left=0;
-        if(row)
-        up=up+getsol(obs,row-1,col,dp);
-        if(col)
-        left=left+getsol(obs,row,col-1,dp);
-        dp[row][col]=left+up;
-        return dp[row][col];
-
+        int up=0,right=0;
+        if(row!=n-1)
+        {
+            up=getans(grid,row+1,col,n,m);
+        }
+        if(col!=m-1)
+        {
+            right=getans(grid,row,col+1,n,m);
+        }
+        return (up+right);
     }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int row=obstacleGrid.size();
-        int col=obstacleGrid[0].size();
-        vector<vector<int>> dp(row, vector<int>(col, -1));
-        return getsol(obstacleGrid,row-1,col-1,dp);
+        int n=obstacleGrid.size();
+        int m=obstacleGrid[0].size();
+        return getans(obstacleGrid,0,0,n,m);
+        // return cnt;
     }
 };
