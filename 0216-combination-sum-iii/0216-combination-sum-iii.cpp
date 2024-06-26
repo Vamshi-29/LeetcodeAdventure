78f0d0c915181d8d82bee062883bc9b7f7d377af
Vamshi <157635591+Vamshi-29@users.noreply.github.com>
2024-06-27 01:08:52 +0530

    Time: 0 ms (100%), Space: 7.9 MB (58.29%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,36 +1,24 @@
 class Solution {
 public:
-    void combi3(vector<vector<int>>&fans,vector<int>&temp,vector<int>&available,int index,int k,int n,vector<int>numbers){
-    if(temp.size()==k)
+    void getsol(vector<int>&temp,int k,int n,int index,          vector<vector<int>>&fans,int sum)
     {
-        int sum=0;
-        for(int i=0;i<k;i++)
-        sum=sum+temp[i];
-        if(sum==n)
-        fans.push_back(temp);
-        return ;
-    }
-    for(int i=index;i<10;i++)
-    {
-        if(available[i])
+        if(temp.size()==k)
         {
-            temp.push_back(numbers[i]);
-            available[i]=0;
-            combi3(fans,temp,available,i+1,k,n,numbers);
-            available[i]=1;
+            if(sum==n)
+            fans.push_back(temp);
+            return ;
+        }
+        for(int i=index;i<=9;i++)
+        {
+            temp.push_back(i);
+            getsol(temp,k,n,i+1,fans,sum+i);
             temp.pop_back();
         }
     }
-    }
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<int>numbers(10,0);
-        for(int i=1;i<=9;i++)
-        numbers[i]=i;
-        vector<int>temp;
-        vector<int>available(10,1);
         vector<vector<int>>fans;
-        combi3(fans,temp,available,1,k,n,numbers);
+        vector<int>temp;
+        getsol(temp,k,n,1,fans,0);
         return fans;
-
     }
 };
