4b76904bf4560b8cf95ff4587072308a2c3b8d15
Vamshi <157635591+Vamshi-29@users.noreply.github.com>
2024-07-01 10:02:46 +0530

    Time: 13 ms (8.06%), Space: 17.1 MB (30.01%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,42 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int startcolor=image[sr][sc];
+        image[sr][sc]=color;
         int m=image.size();
         int n=image[0].size();
-        int startcolor=image[sr][sc];
         if(startcolor==color)
         return image;
-        vector<vector<int>>check(m,vector<int>(n,0));
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(i==sr && j==sc)
-                check[i][j]=color;
-                else
-                check[i][j]=image[i][j];
-            }
-        }
         queue<pair<int,int>>qu;
         qu.push({sr,sc});
-        int drow[4]={-1,0,1,0};
-        int dcol[4]={0,1,0,-1};
+        vector<int>rowarray={1,-1,0,0};
+        vector<int>colarray={0,0,1,-1};
         while(!qu.empty())
         {
-            int crow=qu.front().first;
-            int ccol=qu.front().second;
+            int row=qu.front().first;
+            int col=qu.front().second;
             qu.pop();
             for(int i=0;i<4;i++)
             {
-                int nrow=crow+drow[i];
-                int ncol=ccol+dcol[i];
-                if(nrow>=0 && ncol>=0 && nrow<m && ncol<n && check[nrow][ncol]==startcolor)
+                int drow=row+rowarray[i];
+                int dcol=col+colarray[i];
+                if(drow>=0 && dcol>=0 && drow<m && dcol<n && image[drow][dcol]==startcolor)
                 {
-                    qu.push({nrow,ncol});
-                    check[nrow][ncol]=color;
+                    image[drow][dcol]=color;
+                    qu.push({drow,dcol});
                 }
             }
         }
-        return check;
+        return image;
     }
 };
