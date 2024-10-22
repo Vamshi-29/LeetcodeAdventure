4ec035d73f22117e8d993ac228a3597bb9ff921c
Vamshi <157635591+Vamshi-29@users.noreply.github.com>
2024-10-22 16:46:44 +0530

    Time: 8 ms (99.9%), Space: 157.9 MB (5.08%) - LeetHub

--- 2583-kth-largest-sum-in-a-binary-tree/2583-kth-largest-sum-in-a-binary-tree.cpp
+++ 2583-kth-largest-sum-in-a-binary-tree/2583-kth-largest-sum-in-a-binary-tree.cpp
@@ -1,48 +1,34 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     long long kthLargestLevelSum(TreeNode* root, int k) {
-        queue<TreeNode*>qu;
+        if (!root) return -1;  
+
+        queue<TreeNode*> qu;
         qu.push(root);
-        if(root==nullptr)
-        return -1;
-        priority_queue<int>pq;
-        pq.push(root->val);
-        while(!qu.empty())
-        {
-            int size=qu.size();
-            int sum=0;
-            for(int i=0;i<size;i++)
-            {
-            TreeNode* currnode=qu.front();
-            sum+=currnode->val;
-            qu.pop();
-            if(currnode->left!=nullptr)
-            {
-                qu.push(currnode->left);
+        priority_queue<long long> pq;  
+
+        while (!qu.empty()) {
+            int size = qu.size();
+            long long sum = 0;  
+
+            for (int i = 0; i < size; i++) {
+                TreeNode* currNode = qu.front();
+                sum += currNode->val;
+                qu.pop();
+                if (currNode->left) qu.push(currNode->left);
+                if (currNode->right) qu.push(currNode->right);
             }
-            if(currnode->right!=nullptr)
-            {
-                qu.push(currnode->right);
-            }
-            }
-            pq.push(sum);
+            pq.push(sum);  
         }
-        int size=pq.size();
-        if(size < k)
-        return -1;
-        for(int i=1;i<k;i++)
-        pq.pop();
-        return pq.top();
+
+        
+        if (pq.size() < k) return -1;
+
+        
+        for (int i = 1; i < k; i++) {
+            pq.pop();
+        }
+
+        return pq.top();  
     }
 };
