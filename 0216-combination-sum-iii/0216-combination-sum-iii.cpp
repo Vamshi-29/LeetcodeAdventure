07721cfbd309ee42e402a061412d7dcf7fcadc4c
Vamshi <157635591+Vamshi-29@users.noreply.github.com>
2024-10-29 16:07:40 +0530

    Time: 0 ms (100%), Space: 9.1 MB (20.56%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    void getsol(vector<int>&temp,int k,int n,int index,          vector<vector<int>>&fans,int sum)
+    vector<vector<int>>result;
+    void getsol(int k,int target,int index,vector<int>ans)
     {
-        if(temp.size()==k)
+        if(target<0 || ans.size()>k)
+        return ;
+        if(target==0 && ans.size()==k)
         {
-            if(sum==n)
-            fans.push_back(temp);
+            result.push_back(ans);
             return ;
         }
         for(int i=index;i<=9;i++)
         {
-            temp.push_back(i);
-            getsol(temp,k,n,i+1,fans,sum+i);
-            temp.pop_back();
+            ans.push_back(i);
+            getsol(k,target-i,i+1,ans);
+            ans.pop_back();
         }
     }
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<vector<int>>fans;
-        vector<int>temp;
-        getsol(temp,k,n,1,fans,0);
-        return fans;
+        vector<int>ans;
+        getsol(k,n,1,ans);
+        return result;
     }
 };
