933270e4fe9b272ad5cf6fe0e8f91a6b28071af7
Vamshi <157635591+Vamshi-29@users.noreply.github.com>
2025-02-18 12:51:59 +0530

    Time: 86 ms (5.07%), Space: 10 MB (5.27%) - LeetHub

--- 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.cpp
+++ 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.cpp
@@ -8,23 +8,14 @@ public:
             return;
         }
 
-        if (pattern[pindex] == 'I') {
             for (int i = 1; i <= 9; i++) {
-                if (check[i] == 0 && i > (currans.back() - '0')) {
+                if (check[i] == 0 && (pattern[pindex]=='I' && i > (currans.back() - '0')
+                || (pattern[pindex]=='D' && i< (currans.back()-'0')))) {
                     check[i] = 1;
                     getans(currans + to_string(i), pattern, pindex + 1, check);
                     check[i] = 0;
                 }
             }
-        } else if (pattern[pindex] == 'D') {
-            for (int i = 1; i <= 9; i++) {
-                if (check[i] == 0 && i < (currans.back() - '0')) {
-                    check[i] = 1;
-                    getans(currans + to_string(i), pattern, pindex + 1, check);
-                    check[i] = 0;
-                }
-            }
-        }
     }
 
     string smallestNumber(string pattern) {
