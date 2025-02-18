e8034dcb53b1f11605c89d2be75fc0d99019ed44
Vamshi <157635591+Vamshi-29@users.noreply.github.com>
2025-02-18 13:04:21 +0530

    Time: 0 ms (100%), Space: 8.3 MB (42.39%) - LeetHub

--- 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.cpp
+++ 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
-    string ans = "{";
+    string smallestNumber(string pattern) {
+        string ans;
+        stack<int>st;
 
-    void getans(string currans, string pattern, int pindex, vector<int>& check) {
-        if (currans.length() == pattern.length() + 1) {
-            ans = min(ans, currans);
-            return;
-        }
-
-            for (int i = 1; i <= 9; i++) {
-                if (check[i] == 0 && (pattern[pindex]=='I' && i > (currans.back() - '0')
-                || (pattern[pindex]=='D' && i< (currans.back()-'0')))) {
-                    check[i] = 1;
-                    getans(currans + to_string(i), pattern, pindex + 1, check);
-                    check[i] = 0;
+        for(int i=0;i<=pattern.length();i++)
+        {
+            st.push(i+1);
+            if(i==pattern.length() || pattern[i]=='I')
+            {
+                while(!st.empty()){
+                ans = ans + to_string(st.top());
+                st.pop();
                 }
             }
-    }
-
-    string smallestNumber(string pattern) {
-        vector<int> check(10, 0);
-        for (int i = 1; i <= 9; i++) {
-            check[i] = 1;
-            getans(to_string(i), pattern, 0, check);
-            check[i] = 0;
         }
+
         return ans;
     }
 };
